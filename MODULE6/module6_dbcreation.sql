b3d6840ecab98871cb12a465151bac5bd2ce9d64
champagnedance <47753612+champagnedance@users.noreply.github.com>
2019-07-15 01:48:25 +0300

    module6 homework

---
+++ MODULE6/module6_dbcreation.sql
@@ -0,0 +1,92 @@
+USE master
+GO
+
+DROP DATABASE IF EXISTS SalesOrder
+GO
+
+CREATE DATABASE SalesOrder
+GO
+
+USE SalesOrder
+GO
+
+
+DROP TABLE IF EXISTS supplies, products, details, suppliers
+
+CREATE TABLE suppliers(
+					   supplierID integer PRIMARY KEY,
+					   name varchar(20),
+					   rating integer,
+					   city varchar(20)
+)
+
+INSERT INTO suppliers VALUES
+(1,'Smith',20,'London'),
+(2,'Jonth',10,'Paris'),
+(3,'Blacke',30,'Paris'),
+(4,'Clark',20,'London'),
+(5,'Adams',30,'Athens')
+
+CREATE TABLE details(
+					 detailID integer PRIMARY KEY,
+					 name varchar(20),
+					 color varchar(20),
+					 weight integer,
+					 city varchar(20)
+)
+
+INSERT INTO details VALUES
+(1,'Screw','Red',12,'London'),
+(2,'Bolt','Green',17,'Paris'),
+(3,'Male-screw','Blue',17,'Roma'),
+(4,'Male-screw','Red',14,'London'),
+(5,'Whell','Blue',12,'Paris'),
+(6,'Bloom','Red',19,'London')
+
+CREATE TABLE products(
+					  productID integer PRIMARY KEY,
+					  name varchar(20),
+					  city varchar(20)
+)
+
+INSERT INTO products VALUES
+(1,'HDD','Paris'),
+(2,'Perforator','Roma'),
+(3,'Reader','Athens'),
+(4,'Printer','Athens'),
+(5,'FDD','London'),
+(6,'Terminal','Oslo'),
+(7,'Ribbon','London')
+
+CREATE TABLE supplies(
+					  supplierID integer FOREIGN KEY (supplierID) REFERENCES suppliers(supplierID),
+					  detailID integer FOREIGN KEY (detailID) REFERENCES details(detailID),
+					  productID integer FOREIGN KEY (productID) REFERENCES products(productID),
+					  quantity integer
+) 
+
+INSERT INTO supplies VALUES
+(1,1,1,200),
+(1,1,4,700),
+(2,3,1,400),
+(2,3,2,200),
+(2,3,3,200),
+(2,3,4,500),
+(2,3,5,600),
+(2,3,6,400),
+(2,3,7,800),
+(2,5,2,100),
+(3,3,1,200),
+(3,4,2,500),
+(4,6,3,300),
+(4,6,7,300),
+(5,2,2,200),
+(5,2,4,100),
+(5,5,5,500),
+(5,5,7,100),
+(5,6,2,200),
+(5,1,4,100),
+(5,3,4,200),
+(5,4,4,800),
+(5,5,4,400),
+(5,6,4,500)
---
+++ MODULE6/module6_homework.sql
@@ -0,0 +1,59 @@
+USE SalesOrder
+GO
+
+
+--1
+
+SELECT productID,
+	   [name],
+	   city,
+	   ROW_NUMBER() OVER (order by city) AS ROW_NUM
+FROM products
+
+
+--2
+SELECT productID,
+	   [name],
+	   city,
+	   ROW_NUMBER() OVER (partition by city  order by [name]) AS ROW_NUM
+FROM products
+
+
+--3
+SELECT * FROM (
+SELECT productID,
+	   [name],
+	   city,
+	   ROW_NUMBER() OVER (partition by city  order by [name]) AS ROW_NUM
+FROM products) AS city1
+WHERE ROW_NUM = 1
+
+--4
+SELECT productID,
+	   detailID,
+	   quantity,
+	   SUM(quantity) OVER (PARTITION BY productID order by productID) AS all_quantity_per_prod,
+	   SUM(quantity) OVER (PARTITION BY detailID order by detailID) AS all_quantity_per_det
+FROM supplies
+
+--5
+SELECT * FROM (
+SELECT supplierID,
+	   detailID,
+	   productID,
+	   quantity,
+	   ROW_NUMBER() OVER (order by supplierID) AS rn,
+	   COUNT(*) OVER () AS tot
+FROM supplies) AS total
+WHERE rn BETWEEN 10 AND 15
+
+
+--6
+SELECT * FROM (
+SELECT supplierID,
+	   detailID,
+	   productID,
+	   quantity,
+	   AVG(quantity) OVER () AS avg_qty
+FROM supplies) AS average_quantity_table
+WHERE quantity < avg_qty
